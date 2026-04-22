ce61879de14608bf876d3244dd9168f63e555d4f
Tyra Juliet Clary <tyraclary@arizona.edu>
2026-04-22 14:43:36 -0500

    Create query2.sql Park area by County NV

---
+++ sql/nevada/query2.sql
@@ -0,0 +1,14 @@
+-- Purpose: Calculate total park area by county in Nevada
+
+SELECT
+    c.name AS county_name,
+    ROUND(
+        SUM(ST_Area(ST_Intersection(l.geom, c.geom)::geography)) / 1000000.0,
+        2
+    ) AS park_area_sq_km
+FROM landuse_a AS l
+JOIN adminareas_a AS c
+    ON ST_Intersects(l.geom, c.geom)
+WHERE l.fclass = 'park'
+GROUP BY c.name
+ORDER BY park_area_sq_km DESC;
