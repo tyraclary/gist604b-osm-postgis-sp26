41f12b22bd5559994859d9da99c73a3dd3131a5c
Tyra Juliet Clary <tyraclary@arizona.edu>
2026-04-29 17:43:58 +0000

    sql 3 polygon data

--- sql/nevada/query3.sql
+++ sql/nevada/query3.sql
@@ -6,22 +6,16 @@ WITH major_roads AS (
     WHERE fclass IN ('motorway', 'trunk', 'primary', 'secondary')
 ),
 
-restaurants_near_roads AS (
-    SELECT
-        p.osm_id,
-        p.name,
-        p.geom
-    FROM pois AS p
-    JOIN major_roads AS r
-        ON ST_DWithin(p.geom::geography, r.geom::geography, 500)
-    WHERE p.fclass = 'restaurant'
-)
+-- Purpose: Get land use polygon features in Clark County, Nevada
 
 SELECT
-    c.name AS county_name,
-    COUNT(DISTINCT rnr.osm_id) AS restaurant_count
-FROM restaurants_near_roads AS rnr
+    l.osm_id,
+    l.name,
+    l.fclass,
+    l.geom,
+    c.name AS county_name
+FROM landuse_a AS l
 JOIN adminareas_a AS c
-    ON ST_Within(rnr.geom, c.geom)
-GROUP BY c.name
-ORDER BY restaurant_count DESC;
+    ON ST_Intersects(l.geom, c.geom)
+WHERE c.name = 'Clark County'
+ORDER BY l.fclass, l.name;
